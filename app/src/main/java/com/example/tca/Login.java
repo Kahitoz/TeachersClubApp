98aa1959e5a9a53777ba432bf874dd382a5a958b
Kshitiz Sinha <kshitiz.sinha.025@gmail.com>
2023-06-05 10:29:12 +0530

    first commit

---
+++ app/src/main/java/com/example/tca/Login.java
@@ -0,0 +1,14 @@
+package com.example.tca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ui_login);
+    }
+}
