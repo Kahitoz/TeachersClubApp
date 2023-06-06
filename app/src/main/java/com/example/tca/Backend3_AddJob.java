a47c8720cc57cb1ed63a2873e6b8122e6ecef530
Kshitiz Sinha <kshitiz.sinha.025@gmail.com>
2023-06-06 11:16:38 +0530

    Create Job Screen Completed

---
+++ app/src/main/java/com/example/tca/Backend3_AddJob.java
@@ -0,0 +1,14 @@
+package com.example.tca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Backend3_AddJob extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ui3_add_job);
+    }
+}
