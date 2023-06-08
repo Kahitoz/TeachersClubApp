987f52821256ad84ea54791749c6b4c83b2a9255
Kshitiz Sinha <kshitiz.sinha.025@gmail.com>
2023-06-08 09:37:29 +0530

    Made the ui to handle not logged in page

---
+++ app/src/main/java/com/example/tca/Backend4_LoginMessage.java
@@ -0,0 +1,14 @@
+package com.example.tca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Backend4_LoginMessage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ui4_login_message);
+    }
+}
