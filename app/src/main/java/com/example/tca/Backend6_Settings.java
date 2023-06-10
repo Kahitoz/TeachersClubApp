87934b1d8e4c677a32291a48adf8347a5a0d9efb
Kshitiz Sinha <kshitiz.sinha.025@gmail.com>
2023-06-10 19:36:50 +0530

    Completed the settings page ui

--- app/src/main/java/com/example/tca/Backend6_Settings.java
+++ app/src/main/java/com/example/tca/Backend6_Settings.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Backend4_Notification extends AppCompatActivity {
+public class Backend6_Settings extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ui6_notification);
+        setContentView(R.layout.ui7_settings);
     }
 }
