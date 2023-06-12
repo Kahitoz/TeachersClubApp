c650edbf52502a9d7d584350dd990c9f2ed58abd
Kshitiz Sinha <kshitiz.sinha.025@gmail.com>
2023-06-12 14:33:07 +0530

    completed about job ui

---
+++ app/src/main/java/com/example/tca/Backend10_JobApplicants.java
@@ -0,0 +1,14 @@
+package com.example.tca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Backend10_JobApplicants extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ui10_job_applicants);
+    }
+}
---
+++ app/src/main/java/com/example/tca/Backend11_MessageWindow.java
@@ -0,0 +1,14 @@
+package com.example.tca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Backend11_MessageWindow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ui11_message_window);
+    }
+}
