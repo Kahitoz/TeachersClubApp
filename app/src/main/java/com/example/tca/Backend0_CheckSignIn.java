ebb975075d9f753b2a611db4a1b5611844309f16
Kshitiz Sinha <kshitiz.sinha.025@gmail.com>
2023-06-12 16:19:57 +0530

    Completed the check sign in ui

---
+++ app/src/main/java/com/example/tca/Backend0_CheckSignIn.java
@@ -0,0 +1,14 @@
+package com.example.tca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Backend0_CheckSignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ui0_check_sign_in);
+    }
+}
