2f94dedf4183eef11effbc5414a1241f59c31615
Kshitiz Sinha <kshitiz.sinha.025@gmail.com>
2023-06-13 13:04:53 +0530

    completed the update page ui

--- app/src/main/java/com/example/tca/Backend12_UpdatePage.java
+++ app/src/main/java/com/example/tca/Backend12_UpdatePage.java
@@ -9,6 +9,6 @@ public class Backend12_UpdatePage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_backend12_update_page);
+        setContentView(R.layout.ui12_update_page);
     }
 }
