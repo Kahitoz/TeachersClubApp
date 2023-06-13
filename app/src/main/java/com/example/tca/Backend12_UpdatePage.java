877a5285604f31910e973fc604a42387cb063eab
Kshitiz Sinha <kshitiz.sinha.025@gmail.com>
2023-06-13 12:37:26 +0530

    Added files for the update page

---
+++ app/src/main/java/com/example/tca/Backend12_UpdatePage.java
@@ -0,0 +1,14 @@
+package com.example.tca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Backend12_UpdatePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_backend12_update_page);
+    }
+}
