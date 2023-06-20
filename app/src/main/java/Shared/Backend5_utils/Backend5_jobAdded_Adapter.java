8879c1c81e4b08036a4e314b78758d528385020f
Kshitiz Sinha <kshitiz.sinha.025@gmail.com>
2023-06-20 14:54:48 +0530

    Made some changes

--- app/src/main/java/Shared/Backend5_utils/Backend5_jobAdded_Adapter.java
+++ app/src/main/java/Shared/Backend5_utils/Backend5_jobAdded_Adapter.java
@@ -1,17 +1,21 @@
 package Shared.Backend5_utils;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.tca.R;
+
 public class Backend5_jobAdded_Adapter extends RecyclerView.Adapter<Backend5_jobAdded_Adapter.CardViewHolder> {
 
     @NonNull
     @Override
     public CardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card5_job_posted,parent, false);
+        return new CardViewHolder(view);
     }
 
     @Override
