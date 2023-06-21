c22bb216178c929813521c652aa4179ec883d252
Kshitiz Sinha <kshitiz.sinha.025@gmail.com>
2023-06-21 19:42:37 +0530

    Completed the notification backend and db

--- app/src/main/java/Shared/Backeng6_Utils/Backend6_notification_Adapter.java
+++ app/src/main/java/Shared/Backeng6_Utils/Backend6_notification_Adapter.java
@@ -1,33 +1,60 @@
 package Shared.Backeng6_Utils;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.tca.Backend6_Notification;
+import com.example.tca.R;
+
+import java.util.List;
+
+import Shared.Backend5_utils.Backend5_documents.Backend5_document_Adapter;
 
 public class Backend6_notification_Adapter extends RecyclerView.Adapter<Backend6_notification_Adapter.CardViewHolder> {
+    List<Backend6_notification_GetterSetter> notify;
+    Context context;
+
+    public Backend6_notification_Adapter(List<Backend6_notification_GetterSetter> notify, Context context) {
+        this.notify = notify;
+        this.context = context;
+    }
 
     @NonNull
     @Override
     public Backend6_notification_Adapter.CardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view =  LayoutInflater.from(parent.getContext()).inflate(R.layout.card2_notification, parent, false);
+        return new CardViewHolder(view);
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder(@NonNull Backend6_notification_Adapter.CardViewHolder holder, int position) {
-
+        Backend6_notification_GetterSetter get_set = notify.get(position);
+        holder.b6_message.setText(get_set.getMessage());
+        holder.b6_date.setText("Received on "+get_set.getDate());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return notify.size();
     }
     static class CardViewHolder extends RecyclerView.ViewHolder {
+        TextView b6_name, b6_message, b6_date;
+        ImageView b6_next;
         public CardViewHolder(@NonNull View itemView) {
             super(itemView);
+            b6_name = itemView.findViewById(R.id.textView21);
+            b6_message = itemView.findViewById(R.id.textView22);
+            b6_date = itemView.findViewById(R.id.notify_time);
+            b6_next = itemView.findViewById(R.id.notify_view);
         }
     }
 }
--- app/src/main/java/Shared/Backeng6_Utils/Backend6_notification_handler.java
+++ app/src/main/java/Shared/Backeng6_Utils/Backend6_notification_handler.java
@@ -1,4 +1,79 @@
 package Shared.Backeng6_Utils;
 
+import android.content.Context;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import Shared.Backend5_utils.Backend5_jobsApplied.Backend5_jobApplied_Adapter;
+import Shared.Backend5_utils.Backend5_jobsApplied.Backend5_jobApplied_GetterSetter;
+
 public class Backend6_notification_handler {
-}
+    Context context;
+    FirebaseAuth auth;
+    RecyclerView b6_recycle;
+    FirebaseFirestore database;
+    ProgressBar progressBar;
+
+    public Backend6_notification_handler(Context context, FirebaseAuth auth, RecyclerView b6_recycle, FirebaseFirestore database, ProgressBar progressBar) {
+        this.context = context;
+        this.auth = auth;
+        this.b6_recycle = b6_recycle;
+        this.database = database;
+        this.progressBar = progressBar;
+    }
+
+    public void get_data(){
+        progressBar.setVisibility(View.VISIBLE);
+        database.collection("users")
+                .document(Objects.requireNonNull(auth.getUid()))
+                .collection("notification")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            List<Backend6_notification_GetterSetter> documentList = new ArrayList<>();
+
+                            for (DocumentSnapshot document : task.getResult()) {
+                                Backend6_notification_GetterSetter backend5Document = document.toObject(Backend6_notification_GetterSetter.class);
+                                documentList.add(backend5Document);
+                            }
+
+                            Backend6_notification_Adapter adapter = new Backend6_notification_Adapter(documentList,context);
+                            b6_recycle.setAdapter(adapter);
+                            b6_recycle.setLayoutManager(new LinearLayoutManager(b6_recycle.getContext()));
+
+                        } else {
+                            Toast.makeText(context, "No documents present", Toast.LENGTH_SHORT).show();
+                        }
+                        progressBar.setVisibility(View.GONE);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        progressBar.setVisibility(View.GONE);
+                    }
+                });
+    }
+    }
+
+
