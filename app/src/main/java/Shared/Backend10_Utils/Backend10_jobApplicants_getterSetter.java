b5152dcccc64765ab065346064d4f1065ef3b675
Kshitiz Sinha <kshitiz.sinha.025@gmail.com>
2023-06-22 18:41:55 +0530

    completed the getter and setter part

--- app/src/main/java/Shared/Backend10_Utils/Backend10_jobApplicants_getterSetter.java
+++ app/src/main/java/Shared/Backend10_Utils/Backend10_jobApplicants_getterSetter.java
@@ -1,4 +1,40 @@
 package Shared.Backend10_Utils;
 
 public class Backend10_jobApplicants_getterSetter {
+    String chat, date, status, user_id;
+
+    public Backend10_jobApplicants_getterSetter() {
+    }
+
+    public String getChat() {
+        return chat;
+    }
+
+    public void setChat(String chat) {
+        this.chat = chat;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
 }
