f22f025ecbe4dadb8b300eb912e615dd4a370e82
Kshitiz Sinha <kshitiz.sinha.025@gmail.com>
2023-06-23 21:13:10 +0530

    Created the ui - designs for the view profile

---
+++ app/src/main/java/com/example/tca/Backend13_ViewProfile.java
@@ -0,0 +1,14 @@
+package com.example.tca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Backend13_ViewProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ui13_view_profile);
+    }
+}
